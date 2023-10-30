1ff50c80cebdf64915afe4e57d54c1bfd18a7145
Mohit Sharma <mohit29code@gmail.com>
2023-10-30 20:50:49 +0530

    deployment resolve

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -12,7 +12,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
   if(!productUrl) return;
 
   try {
-    connectToDB();
+    await connectToDB();
 
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
@@ -51,7 +51,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
 export async function getProductById(productId: string) {
   try {
-    connectToDB();
+    await connectToDB();
 
     const product = await Product.findOne({ _id: productId });
 
@@ -65,7 +65,7 @@ export async function getProductById(productId: string) {
 
 export async function getAllProducts() {
   try {
-    connectToDB();
+    await connectToDB();
 
     const products = await Product.find().limit(10);
 
@@ -77,7 +77,7 @@ export async function getAllProducts() {
 
 export async function getSimilarProducts(productId: string) {
   try {
-    connectToDB();
+    await connectToDB();
 
     const currentProduct = await Product.findById(productId);
 
@@ -95,6 +95,7 @@ export async function getSimilarProducts(productId: string) {
 
 export async function addUserEmailToProduct(productId: string, userEmail: string) {
   try {
+    await connectToDB();
     const product = await Product.findById(productId);
 
     if(!product) return;
