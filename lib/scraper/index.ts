8b27e4a527b1672ae4e534abb5e6e67f61a0853d
Mohit Sharma <mohit29code@gmail.com>
2023-12-23 22:46:39 +0530

    api update

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -17,17 +17,16 @@ export async function scrapeAmazonProduct(url: string) {
   }
   const options = {
     method: 'GET',
-    url: 'https://amazon-product-price-data.p.rapidapi.com/product',
+    url: 'https://amazon23.p.rapidapi.com/product-details',
     params: {
-      asins: productId,
-      locale: 'IN'
+      asin: 'B01M72LILF',
+      country: 'IN'
     },
     headers: {
       'X-RapidAPI-Key': '166a2ea0damsha558f4687cfabedp1f2e96jsnf9e38b55de6f',
-      'X-RapidAPI-Host': 'amazon-product-price-data.p.rapidapi.com'
+      'X-RapidAPI-Host': 'amazon23.p.rapidapi.com'
     }
   };
-  
 
   try {
     // Fetch the product page
@@ -43,10 +42,10 @@ export async function scrapeAmazonProduct(url: string) {
     console.log(response.data)
 
     // Extract the product title
-    const title = response.data[0].product_name;
-    const currentPrice = response.data[0].current_price;
+    const title = response.data.result[0].title;
+    const currentPrice = response.data.result[0].price.current_price;
 
-    const originalPrice = response.data[0].current_price;
+    const originalPrice = response.data.result[0].price.before_price;
 
     // const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
 
@@ -55,9 +54,11 @@ export async function scrapeAmazonProduct(url: string) {
     //   $('#landingImage').attr('data-a-dynamic-image') ||
     //   '{}'
 
-    const imageUrls =response.data[0].image_url;
-
-    const currency = response.data[0].currency_symbol;
+    const imageUrls =response.data.result[0].main_image;
+    const discountRate=response.data.result[0].price.saving_percent
+    const currency = response.data.result[0].price.symbol;
+    const category=response.data.result[0].categories[0].category
+    const stock=response.data.result[0].item_available
     // const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
 
     // const description = extractDescription($)
@@ -71,11 +72,11 @@ export async function scrapeAmazonProduct(url: string) {
       currentPrice: currentPrice,
       originalPrice: originalPrice,
       priceHistory: [],
-      discountRate:0,
-      category: 'category',
+      discountRate:discountRate,
+      category: category||'category',
       reviewsCount:100,
       stars: 4.5,
-      isOutOfStock: false,
+      isOutOfStock: stock || false,
       description:"",
       lowestPrice: Number(currentPrice) || Number(originalPrice),
       highestPrice: Number(originalPrice) || Number(currentPrice),
